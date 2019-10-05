b34d0f738f32a82a6d817fd3e09fa156b8c19c95
darkkeks <darkkeks@rambler.ru>
2019-10-05 22:24:04 +0300

    Spring Initializr generated project.

---
+++ src/main/java/ru/darkkeks/isdelectivebackend/IsdElectiveBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.darkkeks.isdelectivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsdElectiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsdElectiveBackendApplication.class, args);
+    }
+
+}
